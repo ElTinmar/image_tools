cf96e9a8d7c553f16ce1f6514679bbb7c9cd317e
ElTinmar <martin.privat@hotmail.fr>
2025-10-29 17:07:53 +0100

    clean init

--- image_tools/__init__.py
+++ image_tools/__init__.py
@@ -4,15 +4,5 @@ from .convert import *
 from .rotation import *
 from .blob_detection import *
 from .roi_selector_widget import *
-from .polyroi import *
 from .GUIs import *
 from .polygons import *
-
-# optional gpu functionalities
-try:
-    from .enhance_gpu import *
-    from .convert_gpu import *
-    from .rotation_gpu import *
-    from .blob_detection_GPU import *
-except:
-    print('image_tools GPU functionalities disabled')
